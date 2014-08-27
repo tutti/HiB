3c9acbfaecd9f3d408b2632d01eff37f76b2f468
Pål Gjerde <paalvaa@gmail.com>
2014-08-27 09:25:06 +0200

    Moving repos

---
+++ DAT100-Oblig-1/src/Velkommen.java
@@ -0,0 +1,17 @@
+/**
+ * Velkommen class
+ * Prints "Bowties are cool." and exits.
+ * 
+ * @author tutti
+ *
+ */
+public class Velkommen {
+	/**
+	 * Main program method. Prints "Bowties are cool."
+	 * 
+	 * @param args Command line arguments. Since this is a Hello World clone, these are ignored.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Bowties are cool.");
+	}
+}
