cff1cb3d7012d4cc780521f1f075a60c63f32e82
Pål Gjerde <paalvaa@gmail.com>
2014-09-01 12:18:26 +0200

    September 1 2014

--- DAT100-Oblig-1/src/Velkommen.java
+++ DAT100-Oblig-1/src/Velkommen.java
@@ -2,16 +2,17 @@
  * Velkommen class
  * Prints "Bowties are cool." and exits.
  * 
- * @author tutti
+ * @author Pål V. Gjerde
  *
  */
 public class Velkommen {
-	/**
-	 * Main program method. Prints "Bowties are cool."
-	 * 
-	 * @param args Command line arguments. Since this is a Hello World clone, these are ignored.
-	 */
-	public static void main(String[] args) {
-		System.out.println("Bowties are cool.");
-	}
+    /**
+     * Main program method. Prints "Bowties are cool."
+     * 
+     * @param args Command line arguments. Since this is a Hello World clone,
+     * these are ignored.
+     */
+    public static void main(String[] args) {
+        System.out.println("Bowties are cool.");
+    }
 }
